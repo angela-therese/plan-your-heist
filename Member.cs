67f6efd7827777c827d7b4965ce8745563c81c8d
User-42 <a.t.iafrate@gmail.com>
2021-04-23 21:53:30 -0400

    Phase 5 of 6 completed

---
+++ Member.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections.Generic;
+
+namespace Heist
+{
+  public class Member
+   {
+       public string Name {get; set;}
+       public int   Level {get; set;}
+       public double Courage {get; set;}
+
+        //  List<Member> HeistMembers = new List<Member>();
+
+    //    public void CreateMember(){
+    //         Console.WriteLine("Plan Your Heist!");
+    //         Console.WriteLine("What is your name?");
+    //         string MemberName = Console.ReadLine();
+    //         Console.WriteLine("Enter a leve 1-10.");
+    //         string MemberLevel = Console.ReadLine();
+    //         int MemberLevelInt = Convert.ToInt32(MemberLevel);
+    //         Console.WriteLine("Enter a number between 0.0 and 2.0");
+    //         string MemberCourage = Console.ReadLine();
+    //         double MemberCourageD = Convert.ToDouble(MemberCourage);
+
+    //        HeistMembers.Add(new Member {
+    //            Name = ($"{MemberName}"),
+    //            Level = MemberLevelInt,
+    //            Courage = MemberCourageD,
+    //        }
+    //        );
+
+    //     Console.WriteLine();
+
+        //    Console.WriteLine($"Current Team Member: {user1.Name}");
+        //    Console.WriteLine($"Member Level: {user1.Level}");
+        //    Console.WriteLine($"Member Courage: {user1.Courage}");
+   }
+   
+   
+}
---
+++ Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Heist
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Member> HeistMembers = new List<Member>();
+
+
+            Console.WriteLine("Plan Your Heist!");
+            while (true)
+            {
+                Console.WriteLine("What is your name?");
+                string MemberName = Console.ReadLine();
+                if (MemberName == "")
+                {
+                    break;
+                }
+                Console.WriteLine("Enter a skill level 1-30.");
+                string MemberLevel = Console.ReadLine();
+                int MemberLevelInt = Convert.ToInt32(MemberLevel);
+                Console.WriteLine("Enter your courage level between 0.0 and 2.0");
+                string MemberCourage = Console.ReadLine();
+                double MemberCourageD = Convert.ToDouble(MemberCourage);
+
+                HeistMembers.Add(new Member
+                {
+                    Name = ($"{MemberName}"),
+                    Level = MemberLevelInt,
+                    Courage = MemberCourageD,
+                });
+
+
+            }
+
+            int BankLevel = 100;
+            int LevelSum = 0;
+            int TrialRuns;
+            int Tries = 0;
+
+
+            Console.WriteLine("How many heists do you want to attempt?");
+            string Heists = Console.ReadLine();
+            TrialRuns = Convert.ToInt32(Heists);
+            Console.WriteLine($"{TrialRuns}");
+
+
+            foreach (Member m in HeistMembers)
+            {
+                LevelSum += m.Level;
+
+            }
+
+            while (true)
+            {
+                if (Tries == TrialRuns)
+                {
+                    break;
+                }
+                else {
+                int secretNumber = new Random().Next(-10, 10);
+                BankLevel = BankLevel + secretNumber;
+
+                if (LevelSum >= BankLevel)
+                {
+                    Console.WriteLine($"Team Skill Level: {LevelSum} Bank Difficulty Level: {BankLevel}");
+                    Console.WriteLine("Heist Success!");
+                }
+                else
+                {
+                    Console.WriteLine($"Team Skill Level: {LevelSum} Bank Difficulty Level: {BankLevel}");
+                    Console.WriteLine("Heist Fail!");
+                }
+                Tries++;
+                Console.WriteLine();
+                }
+            }
+
+
+        }
+    }
+}
